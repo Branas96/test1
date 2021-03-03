6bb1efbeee317cff846150f592d84b5a6a661b14
branas96 <braamvdb96@gmail.com>
2021-03-03 12:24:45 -0700

    Added validation and cookie session

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,13 +6,15 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to root_path, notice: "successful"
+            session[:user_id] = @user.id
+            redirect_to root_path, notice:"Successfully Created!"
         else
             render :new
         end
-        
     end
     
+    private
+
     def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
     end
