3fbda6ed28662a21a4dbf844412a8017c19e58b0
branas96 <braamvdb96@gmail.com>
2021-03-04 11:44:29 -0700

    added user token authentication. note: reset cntrl

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,6 @@
 class PasswordResetsController < ApplicationController
     def def new
-       
+       @user = User.new
     end
     
     def create
@@ -15,4 +15,24 @@ class PasswordResetsController < ApplicationController
       redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"    
     end
     
+    def edit
+      @user = User.find_by(email: params[:email])
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+      redirect_to sign_in_path, alert: "Your Token has Expired!"
+    end
+
+    def update 
+      @user = User.find_by(email: params[:email])
+      if @user.update(password_params)
+        redirect_to sign_in_path, alert: "Your Password was reset Successfully!!. Please Sign In."
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end
