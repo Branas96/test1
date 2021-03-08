1537baa28214f1d4ad2deb8fb8e80f4d5bf4a9cf
branas96 <braamvdb96@gmail.com>
2021-03-08 10:45:36 -0700

    Updated Dependancies in Tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts, dependent: :destroy
+
+    has_many :tweets, dependent: :destroy
+
     has_secure_password
    
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email format"} 
